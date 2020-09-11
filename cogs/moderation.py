8d006029e9a702511fb5485cf82bfb731676ce43
arnavthejaat <arnavchoudhary53@gmail.com>
2020-09-10 22:39:31 -0400

    Fixed ban command

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -24,14 +24,14 @@ class Moderation(commands.Cog):
     async def ban(self, ctx, member: discord.Member, *, reason=None):
         """Bans the member"""
         await ctx.guild.ban(user=member, reason=reason)
-        error = getattr(exception, "original", exception)
-        if isinstance(error, MissingRequiredArgument):
-            await ctx.send(f"""Please specify a member:
-                            For example: **p.ban @memberisdoe**""")
-        else:
-            print(error)
         embd = discord.Embed(title=f"{ctx.author.name} has banned: {member.name}", description=reason, color=0xffff00)
         await ctx.send(embed=embd)
+
+    @ban.error()
+    async def ban_error(self, ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send(f"""Please specify a member to ban:
+                            For example: **p.ban @MemberNoob**""")
 
 
     @commands.command()
