15205442cdf3770833e2099624f5c97c85a4a0b7
arnavthejaat <arnavchoudhary53@gmail.com>
2020-09-27 18:40:34 -0400

    new translator command pog :>

---
+++ cogs/translator.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+import googletrans
+from googletrans import Translator
+
+import datetime as dt
+
+
+class Translate(commands.Cog, name="Commands"):
+    def __init__(self, bot):
+        self.bot = bot
+        self.translator = Translator()
+
+    @commands.command(aliases=["ts"])
+    async def translate(self, ctx, *, text):
+        output = self.translator.translate(str(text), src="auto", dest="en").text
+        embed = discord.Embed()
+        embed.add_field(name="Translated", value=f"{output}", inline=True)
+        await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(Translate(bot))
